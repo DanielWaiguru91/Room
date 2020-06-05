15575f81dbb66f2013c8638a39e280735f90db11
Daniel Waiguru <danielirungu91@gmail.com>
2020-06-05 10:51:14 +0300

    added data binding to buttons to change their text property dynamically

--- app/src/main/java/tech/danielwaiguru/room/ui/StudentViewModel.kt
+++ app/src/main/java/tech/danielwaiguru/room/ui/StudentViewModel.kt
@@ -7,10 +7,24 @@ import tech.danielwaiguru.room.db.StudentDao
 import tech.danielwaiguru.room.db.StudentRepository
 
 class StudentViewModel(private val studentRepository: StudentRepository): ViewModel() {
+    val allStudents = studentRepository.allStudents
     @Bindable
     val inputName = MutableLiveData<String>()
     @Bindable
     val inputCourse = MutableLiveData<String>()
     @Bindable
     val inputDepartment = MutableLiveData<String>()
+
+    /**
+     * Binding the buttons to change the text dynamically
+     */
+    @Bindable
+    val saveOrUpdateBtn = MutableLiveData<String>()
+    @Bindable
+    val deleteOrDeleteAll = MutableLiveData<String>()
+
+    init {
+        saveOrUpdateBtn.value = "Save"
+        deleteOrDeleteAll.value = "Delete All"
+    }
 }
