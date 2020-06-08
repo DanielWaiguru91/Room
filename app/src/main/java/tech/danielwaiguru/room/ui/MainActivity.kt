650c79c83004e86330606b5edcd5dbe4c1596194
Daniel Waiguru <danielirungu91@gmail.com>
2020-06-08 13:27:05 +0300

    added click lister to recyclerview items

--- app/src/main/java/tech/danielwaiguru/room/ui/MainActivity.kt
+++ app/src/main/java/tech/danielwaiguru/room/ui/MainActivity.kt
@@ -3,11 +3,14 @@ package tech.danielwaiguru.room.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import tech.danielwaiguru.room.R
 import tech.danielwaiguru.room.databinding.ActivityMainBinding
+import tech.danielwaiguru.room.db.Student
 import tech.danielwaiguru.room.db.StudentDatabase
 import tech.danielwaiguru.room.db.StudentRepository
 
@@ -40,9 +43,17 @@ class MainActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
         listOfAllStudents()
     }
+    private fun setUpRecyclerView(){
+        binding.studentsRv.layoutManager = LinearLayoutManager(this)
+        listOfAllStudents()
+    }
     private fun listOfAllStudents(){
         studentViewModel.allStudents.observe(this, Observer {
             Log.i("Main", it.toString())
+            binding.studentsRv.adapter = StudentAdapter(it) { selectedItem: Student -> listItemClicked(selectedItem)}
         })
+    }
+    private fun listItemClicked(student: Student){
+        Toast.makeText(this, "Item ${student.name} clicked", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/tech/danielwaiguru/room/ui/StudentAdapter.kt
+++ app/src/main/java/tech/danielwaiguru/room/ui/StudentAdapter.kt
@@ -9,19 +9,22 @@ import tech.danielwaiguru.room.R
 import tech.danielwaiguru.room.databinding.StudentItemBinding
 import tech.danielwaiguru.room.db.Student
 
-class StudentAdapter(private val studentsList: List<Student>): RecyclerView.Adapter<StudentAdapter.StudentViewHolder>() {
+class StudentAdapter(private val studentsList: List<Student>, private val clickListener: (Student) -> Unit): RecyclerView.Adapter<StudentAdapter.StudentViewHolder>() {
     class StudentViewHolder(private val binding: StudentItemBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bind(student: Student){
+        fun bind(student: Student, clickListener: (Student) -> Unit){
             binding.tvName.text = student.name
             binding.tvCourse.text = student.course
             binding.tvDepartment.text = student.department
+            binding.itemLayout.setOnClickListener {
+                clickListener(student)
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding: StudentItemBinding = DataBindingUtil.inflate(layoutInflater, R.layout.student_item, parent, false)
-        return StudentViewHolder((binding))
+        return StudentViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -29,6 +32,6 @@ class StudentAdapter(private val studentsList: List<Student>): RecyclerView.Adap
     }
 
     override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
-        holder.bind(studentsList[position])
+        holder.bind(studentsList[position], clickListener)
     }
 }
